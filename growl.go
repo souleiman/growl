e408367626c854b5e1c7069bd51fb1efe32dd2ea
Souleiman <souleiman@hxdcml.com>
2014-09-10 00:02:23 -0400

    Descent Parser implemented to filter HTML Element <a... or <link...

--- growl.go
+++ growl.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"code.google.com/p/go.net/html"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Growl!~")
+	request, _ := http.Get("http://www.digitalocean.com/")
+	defer request.Body.Close()
+
+	document, err := html.Parse(request.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var descent func(*html.Node)
+	descent = func(node *html.Node) {
+		if node.Type == html.ElementNode && (node.Data == "a" || node.Data == "link") {
+			for _, attr := range(node.Attr) {
+				if attr.Key == "href" {
+					fmt.Println(attr.Val)
+				}
+			}
+		}
+
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			descent(child)
+		}
+	}
+
+	descent(document)
 }
